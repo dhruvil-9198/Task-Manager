9cfaaa0dd38594b3dc7f179867105d91657f46f3
dhruvil-9198 <dkmahida3130@gmail.com>
2024-08-03 12:03:03 +0530

    backend commit

---
+++ backend/server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv')
+const bodyparser = require('body-parser')
+const app = express()
+
+dotenv.config()
+
+const port = 10000
+app.use(cors())
+app.use(bodyparser.json())
+
+const url = process.env.MONGO_URI;
+const client = new MongoClient(url);
+
+const dbName = process.env.DB_NAME;
+client.connect();
+
+app.get('/', async (req, res) => {
+    const name = req.query.u_name;
+    const db = client.db(dbName);
+    const collection = db.collection('Tasks');
+    const findResult = await collection.find({u_name: name}).toArray();
+    res.json(findResult)
+})
+
+app.post('/', async (req, res) => {
+    const task = req.body;
+    const db = client.db(dbName);
+    const collection = db.collection('Tasks');
+    const findResult = await collection.insertOne(task);
+    res.send({success: true, result: findResult})
+})
+
+app.delete('/', async (req, res) => {
+    const task = req.body.id;
+    const db = client.db(dbName);
+    const collection = db.collection('Tasks');
+    const findResult = await collection.deleteOne(({id: task}));
+    res.send({success: true, result: findResult})
+})
+
+// app.post('/', async (req, res) => {
+//     const task = req.body;
+//     const db = client.db(dbName);
+//     const collection = db.collection('Tasks');
+//     const updateResult = await collection.updateOne({_id:task.id}, { $set: { isCompleted: !task.isCompleted }});
+//     res.send({success: true, result: updateResult})
+// })
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+})
